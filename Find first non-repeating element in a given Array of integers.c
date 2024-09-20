11ce566ad790828d9badcdd5102facec0cd3c0d2
AfrinATS <ats.afrin2024@gmail.com>
2024-09-20 12:50:29 +0600

    Add files via upload

---
+++ Find first non-repeating element in a given Array of integers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int count[1000] = {0};
+
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        count[arr[i] + 500]++;
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        if (count[arr[i] + 500] == 1) { // Check count
+            printf("First non-repeating element is: %d\n", arr[i]);
+            return 0;
+        }
+    }
+
+    printf("No non-repeating element found.\n");
+    return 0;
+}
